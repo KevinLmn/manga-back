e432be3935b5744fdaf3da7caecdd2a7dc62eb67
KevinLmn <ikebilol@gmail.com>
2025-04-05 02:05:18 +0200

    fix for deploy

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,25 +1,21 @@
-import { expect } from "chai";
-import request from "supertest";
-import fastify from "../src/index.js";
+import { expect } from 'chai'
+import request from 'supertest'
+import fastify from '../src/index.js'
 
-describe("POST /login", () => {
+describe('POST /login', () => {
   it('should return 401 with "Invalid credentials" for wrong username and password', async () => {
     const response = await request(fastify.server)
-      .post("/login")
-      .send({ username: "wronguser", password: "wrongpass" });
+      .post('/login')
+      .send({ username: 'wronguser', password: 'wrongpass' })
 
-    expect(response.status).to.equal(401);
-    const responseText = "Invalid credentials";
+    expect(response.status).to.equal(401)
+    const responseText = 'Invalid credentials'
     expect(response.text).to.equal(
       `{"success":false,"message":"${responseText}"}`
-    );
+    )
     expect(response.body).to.deep.equal({
       success: false,
-      message: responseText + 1,
-    });
-    // expect(response.body).to.equal({
-    //   success: false,
-    //   message: "Invalid credentials",
-    // });
-  });
-});
+      message: responseText,
+    })
+  })
+})
